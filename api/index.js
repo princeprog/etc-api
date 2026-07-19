04249e58e6d1c896485b24ad810f4f05822d5817
Al Prince Llavan <alprincellavan2019@gmail.com>
2026-07-19 16:29:45 +0800

    fix(api): remove dotenv require from vercel function

--- api/index.js
+++ api/index.js
@@ -1,5 +1,3 @@
-require('dotenv/config');
-
 const { NestFactory } = require('@nestjs/core');
 const { AppModule } = require('../dist/src/app.module');
 const { configureApp } = require('../dist/src/bootstrap');
