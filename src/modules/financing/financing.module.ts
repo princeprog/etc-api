9e63b47e6a191a29f62605d959cd29fb273631f4
Al Prince Llavan <alprincellavan2019@gmail.com>
2026-07-25 15:33:08 +0800

    Fix financing auth module wiring

--- src/modules/financing/financing.module.ts
+++ src/modules/financing/financing.module.ts
@@ -2,12 +2,13 @@ import { Module } from '@nestjs/common';
 
 import { CloudinaryStorageService } from '../../common/storage/cloudinary-storage.service';
 import { ActivityHistoryModule } from '../activity-history/activity-history.module';
+import { AuthModule } from '../auth/auth.module';
 import { FinancingController } from './financing.controller';
 import { FinancingService } from './financing.service';
 import { PublicFinancingUploadsController } from './public-financing-uploads.controller';
 
 @Module({
-  imports: [ActivityHistoryModule],
+  imports: [ActivityHistoryModule, AuthModule],
   controllers: [FinancingController, PublicFinancingUploadsController],
   providers: [FinancingService, CloudinaryStorageService],
   exports: [FinancingService],
